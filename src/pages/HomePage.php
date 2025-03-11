b9ae46c854a94b2d57d1631b6c51bf592592905f
fitri-hy <noreply.orzpartners@gmail.com>
2025-03-11 15:02:36 +0700

    update

--- src/pages/HomePage.php
+++ src/pages/HomePage.php
@@ -17,7 +17,7 @@ class HomePage {
 					[img class:'logo' width:'100' height:'100' src:'/images/logo/logo.png' alt:'logo']
 					[h1 class:'title']PHX Framework[/h1]
 					[p class:'subtitle']Fast | Secure | Powerful[/p]
-					[a class:'link' href:'https://github.com/fitri-hy/phx' target:'_blank']Github[/a]
+					[a class:'link' href:'https@github.com/fitri-hy/phx' target:'_blank']Github[/a]
 				[/div]
 			[/div]
         ");
